705d5630b69035666258b153c8a57a020cd07b6b
yiliyili <1724335344@qq.com>
2020-06-24 19:06:32 +0800

    第二题加注释

--- AddTwoNumbers.php
+++ AddTwoNumbers.php
@@ -23,8 +23,8 @@ class Solution {
    */
   function addTwoNumbers($l1, $l2) {
     $add = 0;
-    $list = new ListNode(0);
-    $cur = $list;
+    $list = new ListNode(0);//增加一个当前虚拟头结点
+    $cur = $list;//相当于复制一个指针,list始终指向虚拟头结点,只是cur在不断变化
 
     while ($l1 || $l2) {
       $x = $l1 != null ? $l1->val : 0;
@@ -32,20 +32,20 @@ class Solution {
 
       $val = ($x + $y + $add) % 10;
 
-      $add = floor(($x + $y + $add ) / 10);
+      $add = floor(($x + $y + $add ) / 10);//有没有进位
 
       $new = new ListNode($val);
       $cur->next = $new;
       $cur = $cur->next; 
 
-      if ($l1 != null) {
+      if ($l1 != null) {//l1没到尾部
         $l1 = $l1->next;
       }
       if ($l2 != null) {
         $l2 = $l2->next;
       }
     }
-    if ($add > 0) {
+    if ($add > 0) {//可能最后一个链表要进位
       $cur->next = new ListNode($add);
     }
     return $list->next;
