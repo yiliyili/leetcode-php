3ecfb3e3469921a36f6f98d9b637127cc8a88273
yiliyili <1724335344@qq.com>
2020-06-30 16:57:22 +0800

    84,85单调栈

--- LargestRectangleinHistogram.php
+++ LargestRectangleinHistogram.php
@@ -1,26 +1,34 @@
 <?php
+// 84. 柱状图中最大的矩形
 class Solution {
-
+//   输入: [2,1,5,6,2,3]
+// 输出: 10
+// 比如1会拖2的后退,有1在,2最大也大不到那里去,最多能用到高度1
   /**
    * @param Integer[] $heights
    * @return Integer
    */
   function largestRectangleArea($heights) {
-    array_push($heights, 0);
+    array_push($heights, 0);//避免一直递增没法计算面积,
     $stack = new SplStack;
     $max = 0;
     for ($i = 0; $i < count($heights); $i++) {
-      while (!$stack->isEmpty() && $heights[$stack->top()] >= $heights[$i]) {
-        $last = $stack->pop();
+      while (!$stack->isEmpty() && $heights[$stack->top()] >= $heights[$i]) {//计算i之前的面积
+        $last = $stack->pop();//pop出栈顶下标
         if (!$stack->isEmpty()) {
-          $width = $i - $stack->top() - 1;
+          $width = $i - $stack->top() - 1;//注意里边放的不是高度而是下标
+          echo '$i = '.$i. ' top='. $stack->top() . 'width='. $width .'<br/>';
+          // 第一次算出来的是6,第二次while才是5*宽度2=10
         } else { 
           $width = $i;
         }
         $max = max($max, $heights[$last] * $width);
       }
-      $stack->push($i);
+      $stack->push($i);//单调递增栈,可能后边还有更高的柱子,造出更大的矩形,入栈下标用于算宽度
     }
     return $max;
   }
 }
+
+$solu = new Solution();
+var_dump( $solu->largestRectangleArea([2,1,5,6,6,2,3]));
--- MaximalRectangle.php
+++ MaximalRectangle.php
@@ -1,4 +1,5 @@
 <?php
+// 85. 最大矩形
 class Solution {
 
   /**
@@ -12,13 +13,13 @@ class Solution {
       ["1","1","1","1","1"],
       ["1","0","0","1","0"]
     ]
-
+每一行看做是84题类似的赵最大矩形
 
 
     [1,0,1,0,0],
-    [2,0,2,1,1],
+    [2,0,2,1,1],  把前一行的累加上来
     [3,1,3,2,2],
-    [4,0,0,3,0]
+    [4,0,0,3,0] //是0就不累加
   
    */
   function maximalRectangle($matrix) {
@@ -27,16 +28,16 @@ class Solution {
     for ($i = 0; $i < count($matrix); $i++) {
       for ($j = 0; $j < count($matrix[0]); $j++) {
         if ($i == 0) {
-          $heights[$j] = $matrix[$i][$j] == 0 ? 0 : 1;
+          $heights[$j] = $matrix[$i][$j] == 0 ? 0 : 1;//不是0就是1,第一行也不进行累加
         } else {
-          $heights[$j] = $matrix[$i][$j] == 0 ? 0 : (1 + $heights[$j]);
+          $heights[$j] = $matrix[$i][$j] == 0 ? 0 : (1 + $heights[$j]);//高度为0,不累加前边的
         }
       }
-      $res = max($res, $this->area($heights));
+      $res = max($res, $this->area($heights));//heights就是84题的参数,每一行进行代入
     }
     return $res;
   }
-
+  //这是84题解题办法
   function area($heights) {
     array_push($heights, 0);
     $stack = new SplStack;
