5873403ac161284b0360efb11cae418f7db54e1f
yiliyili <1724335344@qq.com>
2020-07-31 12:17:14 +0800

    300加注释

--- LongestIncreasingSubsequence.php
+++ LongestIncreasingSubsequence.php
@@ -39,6 +39,25 @@ class Solution {
     $this->LIS[$l] = $num;
   }
 
+  //更好的
+  function lengthOfLIS3($nums) {
+        $len = count($nums);
+        if ($len <= 1) return $len;
+
+        // dp 数组的含义，表示  [0,...,i] 子串的 LIS，所以最后返回 max(dp) 即可
+        $dp = array_fill(0, $len, 1);
+        for ($i = 1; $i < $len; ++$i) {
+            // i 是当前下标，j 是比 i 小的所有下标
+            for ($j = 0; $j < $i; ++$j) {
+                if ($nums[$j] < $nums[$i]) {
+                    $dp[$i] = max($dp[$i], $dp[$j] + 1);//内层循环固定了一个i后,不断找以$nums[$i]结尾的最长上升子序列
+                }
+            }
+        }
+
+        return max($dp);
+    }
+
 
   //动态规划 dp[i]表示以 nums[i] 结尾的「上升子序列」的长度
   function lengthOfLIS($nums) {
@@ -58,24 +77,7 @@ class Solution {
     return $max;
   }
 
-  //更好的
-  function lengthOfLIS3($nums) {
-        $len = count($nums);
-        if ($len <= 1) return $len;
-
-        // dp 数组的含义，表示  [0,...,i] 子串的 LIS，所以最后返回 max(dp) 即可
-        $dp = array_fill(0, $len, 1);
-        for ($i = 1; $i < $len; ++$i) {
-            // i 是当前下标，j 是比 i 小的所有下标
-            for ($j = 0; $j < $i; ++$j) {
-                if ($nums[$j] < $nums[$i]) {
-                    $dp[$i] = max($dp[$i], $dp[$j] + 1);
-                }
-            }
-        }
-
-        return max($dp);
-    }
+  
 
 
 
