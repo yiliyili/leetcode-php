9b4a0483d56f083149c2d9092ec0c10c07e2090b
yiliyili <1724335344@qq.com>
2020-10-16 23:43:15 +0800

    增加题解168 171

---
+++ 168.php
@@ -0,0 +1,27 @@
+<?php
+// 168. Excel表列名称
+// 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+//  3 -> C
+    // ...
+    // 26 -> Z
+    // 27 -> AA
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String
+     */
+    function convertToTitle($n) {
+        if($n<=0) return "";
+        $s = "";
+        while($n > 0){
+            $n --;
+            $s = chr(($n%26) + 65).$s;
+            $n = (int) ($n/26);
+        }
+        return $s;
+    }
+}
+
+$solu = new Solution();
+var_dump($solu->convertToTitle(27));//AA
---
+++ 171.php
@@ -0,0 +1,25 @@
+<?php
+// 171. Excel表列序号
+// 给定一个Excel表格中的列名称，返回其相应的列序号。
+ // C -> 3
+ //    ...
+ //    Z -> 26
+ //    AA -> 27
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function titleToNumber($s) {
+        $n = strlen($s);
+        $ans = 0;
+        for($i=0;$i<$n;$i++){
+            $ans += (ord($s[$i])-64)*pow(26,$n-$i-1);
+        }
+        return $ans;
+    }
+}
+
+$solu = new Solution();
+var_dump($solu->titleToNumber('AA'));//27
